f32f343711b027bd665b6f18b50a69a651d2af49
eswestra <e.s.westra.95@gmail.com>
2022-01-01 21:50:44 +0100

    day1 complete

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
+	log.Print("----------")
+	log.Println("Day 1:")
 	day1puzzle1, day1puzzle2 := day1.SolveDay1()
 	log.Printf("day1puzzle1 - amount of increases found : %d", day1puzzle1)
 	log.Printf("day1puzzle2 - amount of increases found : %d", day1puzzle2)
+	log.Print("----------")
 }
