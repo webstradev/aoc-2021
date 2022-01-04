cb8d339c7f2bd2706b0e86e570dbdd12adafa380
webstradev <e.s.westra.95@gmail.com>
2022-01-04 23:17:40 +0100

    ADD function to parse a game from an input file (+tests)

--- day4/day4.go
+++ day4/day4.go
@@ -1,5 +1,18 @@
 package day4
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// A game contains the numbers to draw and a list of bingo boards
+type Game struct {
+	numbers []int
+	boards  []Board
+}
+
 // A board is a 5x5 matrix of ints
 type Board [5][5]int
 
@@ -40,4 +53,76 @@ func isBoardWinning(board Board) bool {
 
 	// No winning rows or columns found:
 	return false
+}
+
+// splitStringProperly splits a string and ignores empty strings and new line symbols
+func splitStringProperly(s, delim string) []string {
+	ss := strings.Split(s, delim)
+	var ns []string
+	// Loop over each value after the split and only include it in the result if it isn't empty
+	for _, v := range ss {
+		if v == "" || v == "\n" {
+			continue
+		}
+		ns = append(ns, v)
+	}
+	return ns
+}
+
+// parseGameFromInput parse the game numbers and board from an input file
+func parseGameFromInput(path string) (*Game, error) {
+	var game Game
+
+	// Read the file
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	// Create a new scanner to loop over lines
+	fs := bufio.NewScanner(f)
+
+	// Select the first line (this will contain the game input)
+	fs.Scan()
+
+	// Loop over the numbers on the line and parse them to ints, then add them to the game
+	for _, v := range strings.Split(fs.Text(), ",") {
+		parsed, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		game.numbers = append(game.numbers, parsed)
+	}
+
+	// Scan over the remaining lines
+	for fs.Scan() {
+		var board Board
+
+		// Ignore whitespace/ empty lines (these are used to seperate boards)
+		if fs.Text() == " " || fs.Text() == "\n" || fs.Text() == "" {
+			continue
+		}
+
+		// If we reach here we have reached the start of a board
+
+		// Loop over the 5 rows of the board
+		for x := 0; x < 5; x++ {
+			// Loop over the items on the row (uses splitStringProperly to ignore extra spaces and whitespaces)
+			for y, value := range splitStringProperly(fs.Text(), " ") {
+				parsed, err := strconv.Atoi(string(value))
+				if err != nil {
+					return nil, err
+				}
+				// Add value to the board at the correct position
+				board[x][y] = parsed
+			}
+			// Move the pointer to the next line so it doesn't get run again when we finish the board loop
+			fs.Scan()
+		}
+		// Add the board to the game
+		game.boards = append(game.boards, board)
+	}
+	return &game, nil
 }
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -6,48 +6,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var sumUnmarkedValuesTests = []struct {
-	name  string
-	board Board
-	sum   int
-}{
-	{
-		"sumBoard1",
-		Board{
-			{-1, 13, 17, 11, -1},
-			{8, -1, 23, 4, 24},
-			{21, 9, -1, 16, 7},
-			{6, -1, 3, -1, 5},
-			{1, 12, -1, 15, -1},
-		},
-		195,
-	},
-	{
-		"sumBoard2",
-		Board{
-			{-1, 15, 0, -1, 22},
-			{-1, 18, 13, -1, 5},
-			{-1, 8, -1, 25, 23},
-			{-1, 11, -1, 24, 4},
-			{-1, 21, 16, 12, -1},
-		},
-		217,
-	},
-	{
-		"sumBoard3",
-		Board{
-			{-1, -1, -1, -1, -1},
-			{10, 16, 15, -1, 19},
-			{18, 8, -1, 26, 20},
-			{22, -1, 13, 6, -1},
-			{-1, -1, 12, 3, -1},
-		},
-		188,
-	},
-}
-
 func TestSumBoard(t *testing.T) {
-	for _, test := range sumUnmarkedValuesTests {
+	tests := []struct {
+		name  string
+		board Board
+		sum   int
+	}{
+		{
+			"sumBoard1",
+			Board{
+				{-1, 13, 17, 11, -1},
+				{8, -1, 23, 4, 24},
+				{21, 9, -1, 16, 7},
+				{6, -1, 3, -1, 5},
+				{1, 12, -1, 15, -1},
+			},
+			195,
+		},
+		{
+			"sumBoard2",
+			Board{
+				{-1, 15, 0, -1, 22},
+				{-1, 18, 13, -1, 5},
+				{-1, 8, -1, 25, 23},
+				{-1, 11, -1, 24, 4},
+				{-1, 21, 16, 12, -1},
+			},
+			217,
+		},
+		{
+			"sumBoard3",
+			Board{
+				{-1, -1, -1, -1, -1},
+				{10, 16, 15, -1, 19},
+				{18, 8, -1, 26, 20},
+				{22, -1, 13, 6, -1},
+				{-1, -1, 12, 3, -1},
+			},
+			188,
+		},
+	}
+
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got := sumUnMarkedValues(test.board)
 			assert.Equal(t, test.sum, got, "Calculated sum was unexpected")
@@ -55,50 +55,105 @@ func TestSumBoard(t *testing.T) {
 	}
 }
 
-var isBoardWinningTests = []struct {
-	name    string
-	board   Board
-	winning bool
-}{
-	{
-		"Winning board - horizontal win",
-		Board{
-			{-1, -1, -1, -1, -1},
-			{10, 16, 15, -1, 19},
-			{18, 8, -1, 26, 20},
-			{22, -1, 13, 6, -1},
-			{-1, -1, 12, 3, -1},
-		},
-		true,
-	},
-	{
-		"Winning board - vertical win",
-		Board{
-			{-1, 15, 0, -1, 22},
-			{-1, 18, 13, -1, 5},
-			{-1, 8, -1, 25, 23},
-			{-1, 11, -1, 24, 4},
-			{-1, 21, 16, 12, -1},
-		},
-		true,
-	}, {
-		"Losing board",
-		Board{
-			{-1, 15, 0, -1, 22},
-			{15, 18, 13, -1, 5},
-			{-1, 8, -1, 25, 23},
-			{-1, 11, -1, 24, 4},
-			{-1, 21, 16, 12, -1},
-		},
-		false,
-	},
-}
-
 func TestIsBoardWinning(t *testing.T) {
-	for _, test := range isBoardWinningTests {
+	tests := []struct {
+		name    string
+		board   Board
+		winning bool
+	}{
+		{
+			"Winning board - horizontal win",
+			Board{
+				{-1, -1, -1, -1, -1},
+				{10, 16, 15, -1, 19},
+				{18, 8, -1, 26, 20},
+				{22, -1, 13, 6, -1},
+				{-1, -1, 12, 3, -1},
+			},
+			true,
+		},
+		{
+			"Winning board - vertical win",
+			Board{
+				{-1, 15, 0, -1, 22},
+				{-1, 18, 13, -1, 5},
+				{-1, 8, -1, 25, 23},
+				{-1, 11, -1, 24, 4},
+				{-1, 21, 16, 12, -1},
+			},
+			true,
+		}, {
+			"Losing board",
+			Board{
+				{-1, 15, 0, -1, 22},
+				{15, 18, 13, -1, 5},
+				{-1, 8, -1, 25, 23},
+				{-1, 11, -1, 24, 4},
+				{-1, 21, 16, 12, -1},
+			},
+			false,
+		},
+	}
+
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got := isBoardWinning(test.board)
 			assert.Equal(t, test.winning, got, "The function did not return an expected winning bool")
+		})
+	}
+}
+
+func TestParseGameFromInput(t *testing.T) {
+	tests := []struct {
+		name     string
+		path     string
+		expected *Game
+		success  bool
+	}{
+		{
+			"Invalid Path",
+			"./test.xyz",
+			nil,
+			false,
+		},
+		{
+			"Valid Input",
+			"./input_test.txt",
+			&Game{
+				[]int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1},
+				[]Board{
+					{
+						{22, 13, 17, 11, 0},
+						{8, 2, 23, 4, 24},
+						{21, 9, 14, 16, 7},
+						{6, 10, 3, 18, 5},
+						{1, 12, 20, 15, 19},
+					},
+					{
+						{3, 15, 0, 2, 22},
+						{9, 18, 13, 17, 5},
+						{19, 8, 7, 25, 23},
+						{20, 11, 10, 24, 4},
+						{14, 21, 16, 12, 6},
+					},
+					{
+						{14, 21, 17, 24, 4},
+						{10, 16, 15, 9, 19},
+						{18, 8, 23, 26, 20},
+						{22, 11, 13, 6, 5},
+						{2, 0, 12, 3, 7},
+					},
+				},
+			},
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := parseGameFromInput(test.path)
+			assert.Equal(t, test.success, err == nil, "Expected success does not match error state")
+			assert.Equal(t, test.expected, got, "The function did not return the expected game")
 		})
 	}
 }
